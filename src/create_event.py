be5b690c63114c24395f33b7fabbd9c8bda8b42c
Preethi <preethivaid@gmail.com>
2018-06-02 19:08:27 -0400

    Have ability to create an event with summary from input prompt

---
+++ src/create_event.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+
+from pathlib import Path
+
+import httplib2
+from apiclient import discovery
+from dotenv import load_dotenv
+
+from src.credentials import GetCredentials as Cred
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+class CreateEvent:
+
+    def event_prompt(self):
+        summary_input = input("Event summary: ")
+        self.create_event(summary_input)
+
+    @staticmethod
+    def create_event(summary):
+        """Shows basic usage of the Google Calendar API.
+
+        Creates a Google Calendar API service object and outputs a list of the next
+        10 events on the user's calendar.
+        """
+        credentials = Cred.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
+
+        event = {
+            'summary': summary,
+            'location': '800 Howard St., San Francisco, CA 94103',
+            'description': 'Preethi Test Event.',
+            'start': {
+                'dateTime': '2018-06-05T09:00:00-07:00',
+                'timeZone': 'America/Los_Angeles',
+            },
+            'end': {
+                'dateTime': '2018-06-05T17:00:00-07:00',
+                'timeZone': 'America/Los_Angeles',
+            },
+            'recurrence': [
+                'RRULE:FREQ=DAILY;COUNT=1'
+            ],
+            'attendees': [
+                {'email': 'tyler@example.com'},
+            ],
+            'reminders': {
+                'useDefault': False,
+                'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10},
+                ],
+            },
+        }
+
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))
+
+
+if __name__ == '__main__':
+    CreateEvent().event_prompt()
---
+++ src/credentials.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import os
+
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+from dotenv import load_dotenv
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+class GetCredentials:
+
+    @staticmethod
+    def get_credentials():
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'pv-calendar.json')
+
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+
+            tmp_secret_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'google_api_secret.json')
+            with open(tmp_secret_file, 'w+') as api_file:
+                api_file.write(os.getenv('GOOGLE_API_SECRET'))
+            flow = client.flow_from_clientsecrets(tmp_secret_file, SCOPES)
+            os.remove(tmp_secret_file)
+            flow.user_agent = APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else:  # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
