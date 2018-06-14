80d448d7b2984c4788a91cef3dd554838c043a8f
Preethi <preethivaid@gmail.com>
2018-06-13 20:00:56 -0400

    Only allow texts from my number

--- src/sms_handler.py
+++ src/sms_handler.py
@@ -9,15 +9,18 @@ app = Flask(__name__)
 def sms():
     sms_request = request.form['Body']
     sms_response = MessagingResponse()
-    if "summary" in sms_request.split(" ")[0].lower():
-        calendar_summary_text = CalendarHandler().get_summary(sms_request)
-        sms_response.message(calendar_summary_text)
-    elif "delete" in sms_request.split(" ")[0].lower():
-        calendar_summary_text = CalendarHandler().delete_event()
-        sms_response.message(calendar_summary_text)
+    if request.form['From'] == '+18604024490':
+        if "summary" in sms_request.split(" ")[0].lower():
+            calendar_summary_text = CalendarHandler().get_summary(sms_request)
+            sms_response.message(calendar_summary_text)
+        elif "delete" in sms_request.split(" ")[0].lower():
+            calendar_summary_text = CalendarHandler().delete_event()
+            sms_response.message(calendar_summary_text)
+        else:
+            response_text = CalendarHandler().create_event(sms_request)
+            sms_response.message(response_text)
     else:
-        response_text = CalendarHandler().create_event(sms_request)
-        sms_response.message(response_text)
+        sms_response.message('Nice try buddy!')
     return str(sms_response)
 
 
