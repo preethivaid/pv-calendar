aa20c71b82cad899bb891a086cfa831f71c67cdd
Preethi <preethivaid@gmail.com>
2018-06-13 20:52:35 -0400

    Fix typo

--- src/sms_handler.py
+++ src/sms_handler.py
@@ -9,6 +9,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from src.calendar_handler import CalendarHandler
+from src.sheets_handler import SheetsHandler
 from src.utils import format_message
 
 app = Flask(__name__)
@@ -18,18 +19,22 @@ app = Flask(__name__)
 def sms():
     sms_request = request.form['Body']
     sms_response = MessagingResponse()
-    if request.form['From'] == '+18604024490':
-        if "summary" in sms_request.split(" ")[0].lower():
-            calendar_summary_text = CalendarHandler().get_summary(sms_request)
-            sms_response.message(calendar_summary_text)
-        elif "delete" in sms_request.split(" ")[0].lower():
-            calendar_summary_text = CalendarHandler().delete_event(sms_request)
-            sms_response.message(calendar_summary_text)
-        else:
-            response_text = CalendarHandler().create_event(sms_request)
-            sms_response.message(response_text)
-    else:
-        sms_response.message(format_message('Nice try buddy!'))
+
+    SheetsHandler().call_sheets()
+
+
+    # if request.form['From'] == '+18604024490':
+    #     if "summary" in sms_request.split(" ")[0].lower():
+    #         calendar_summary_text = CalendarHandler().get_summary(sms_request)
+    #         sms_response.message(calendar_summary_text)
+    #     elif "delete" in sms_request.split(" ")[0].lower():
+    #         calendar_summary_text = CalendarHandler().delete_event(sms_request)
+    #         sms_response.message(calendar_summary_text)
+    #     else:
+    #         response_text = CalendarHandler().create_event(sms_request)
+    #         sms_response.message(response_text)
+    # else:
+    #     sms_response.message(format_message('Nice try buddy!'))
     return str(sms_response)
 
 
